b3b6c7d4480cf5884330a0b69b49303848d0034c
Max <rantil@pm.me>
2022-03-10 12:21:19 +0200

    structure leo_agent

--- leo_agent/movement.c
+++ leo_agent/movement.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "agent.h"
+#include "../src/agent.h"
 #include "beenet.h"
 
 dir_t	get_player_dir(agent_info_t *info, int hasflower)
@@ -16,7 +16,7 @@ dir_t	get_player_dir(agent_info_t *info, int hasflower)
 	if (hasflower)
 	{
 		if (info->row > game.hivecords.row && bee_pos_is_midmap)
-			dir = NW + (flag_dir[dir] * (info->player == 1));	
+			dir = NW + (flag_dir[dir] * (info->player == 1));
 		if (info->row < game.hivecords.row && bee_pos_is_midmap)
 			dir = SW + (flag_dir[dir] * (info->player == 1));
 		if (info->col == game.hivecords.col && info->row > 12)
