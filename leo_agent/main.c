b9b36050386dc6aada118ad8198451beb544b1d8
Leo MPB <leotran@student.hive.fi>
2022-03-10 13:37:54 +0200

    updated agent

--- leo_agent/main.c
+++ leo_agent/main.c
@@ -3,13 +3,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "agent.h"
+#include "../src/agent.h"
 #include "beenet.h"
 
 t_brains	brains[5];
-t_game 		game;
-
-
 
 t_game		init_game()
 {
@@ -145,26 +142,42 @@ int find_neighbour(agent_info_t info, cell_t type)
     return -1;
 }
 
+int	is_obstacle(agent_info_t info)
+{
+	cell_t obstacle = 0;
+
+	for (int i = 1; i < 6; i++)
+	{
+		if (find_neighbour(info, obstacle + i) >= 0)
+			return (1);
+	}
+	if (find_neighbour(info, OUTSIDE) >= 0)
+		return (1);	
+	return (0);
+}
+
 dir_t	get_player_dir(agent_info_t *info, int hasflower)
 {
 	dir_t dir;
 	int bee_pos_is_midmap = (info->col > 1 && info->col < 28);
 	int go_way = rand() % 8;
-	if (go_way == W)
+	if (go_way == W && info->player == 0)
 		go_way = E;
-	if (hasflower)
+	if (go_way == E && info->player == 1)
+		go_way = W;
+	if (hasflower && is_obstacle(*info) == 0)
 	{
 		if (info->row > game.hivecords.row && bee_pos_is_midmap)
-			dir = NW + (flag_dir[dir] * (info->player == 1));	
-		if (info->row < game.hivecords.row && bee_pos_is_midmap)
-			dir = SW + (flag_dir[dir] * (info->player == 1));
-		if (info->col == game.hivecords.col && info->row > 12)
+			dir = NW + flag_dir[NW] * (info->player == 1);	
+		else if (info->row < game.hivecords.row && bee_pos_is_midmap)
+			dir = SW + flag_dir[SW] * (info->player == 1);
+		else if (info->col == game.hivecords.col && info->row > 12)
 			dir = N;
-		if (info->col == game.hivecords.col && info->row < 12)
+		else if (info->col == game.hivecords.col && info->row < 12)
 			dir = S;
-		if (info->col > game.hivecords.col && info->player == 0)
+		else if (info->col > game.hivecords.col && info->player == 0)
 			dir = W;
-		if (info->col < game.hivecords.col && info->player == 1)
+		else if (info->col < game.hivecords.col && info->player == 1)
 			dir = E;
 	}
 	else
@@ -176,9 +189,10 @@ command_t think(agent_info_t info)
 {
     cell_t bee = info.cells[VIEW_DISTANCE][VIEW_DISTANCE];
 
-	update_map(game, info);
+	//update_map(game, info);
 	//print_map(game);
-	//printf("row: %d\n col: %d\n", game.hivecords.row, game.hivecords.col);
+	game.hivecords.row = 12;
+	game.hivecords.col = 1 + 27 * (info.player == 1);
     if (is_bee_with_flower(bee))
     {
         int hive_dir = find_neighbour(info, hive_cell(info.player));
@@ -227,9 +241,9 @@ command_t think(agent_info_t info)
 
 int main(int argc, char **argv)
 {
-	game = init_game();
+	//game = init_game();
 
-    init_brains();
+   // init_brains();
     if (argc < 3)
         panic("Usage: ./agent arena_host arena_ip");
 
