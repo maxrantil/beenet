36c325feb877f94edc03888f778374b788e78b49
Max <rantil@pm.me>
2022-03-10 15:02:48 +0200

    leo_agent working max correct

--- leo_agent/main.c
+++ leo_agent/main.c
@@ -142,49 +142,6 @@ int find_neighbour(agent_info_t info, cell_t type)
     return -1;
 }
 
-/* int	is_obstacle(agent_info_t info)
-{
-	cell_t obstacle = 0;
-
-	for (int i = 1; i < 6; i++)
-	{
-		if (find_neighbour(info, obstacle + i) >= 0)
-			return (1);
-	}
-	if (find_neighbour(info, OUTSIDE) >= 0)
-		return (1);
-	return (0);
-} */
-
-/* dir_t	get_player_dir(agent_info_t *info, int hasflower)
-{
-	dir_t dir;
-	int bee_pos_is_midmap = (info->col > 1 && info->col < 28);
-	int go_way = rand() % 8;
-	if (go_way == W && info->player == 0)
-		go_way = E;
-	if (go_way == E && info->player == 1)
-		go_way = W;
-	if (hasflower && is_obstacle(*info) == 0)
-	{
-		if (info->row > game.hivecords.row && bee_pos_is_midmap)
-			dir = NW + flag_dir[NW] * (info->player == 1);
-		else if (info->row < game.hivecords.row && bee_pos_is_midmap)
-			dir = SW + flag_dir[SW] * (info->player == 1);
-		else if (info->col == game.hivecords.col && info->row > 12)
-			dir = N;
-		else if (info->col == game.hivecords.col && info->row < 12)
-			dir = S;
-		else if (info->col > game.hivecords.col && info->player == 0)
-			dir = W;
-		else if (info->col < game.hivecords.col && info->player == 1)
-			dir = E;
-	}
-	else
-		dir = go_way + (flag_dir[dir] * (info->player == 1));
-	return (dir);
-} */
-
 command_t think(agent_info_t info)
 {
     cell_t bee = info.cells[VIEW_DISTANCE][VIEW_DISTANCE];
@@ -219,8 +176,26 @@ command_t think(agent_info_t info)
     }
 	if (brains[info.bee].hasflower == true)
 	{
-		int obstacle;
-		obstacle = find_neighbour(info, FLOWER);
+		int obstacle = -1;
+
+		if (find_neighbour(info, FLOWER) >= 0)
+			obstacle = 1;
+		else if (find_neighbour(info, OUTSIDE) >= 0)
+			obstacle = 1;
+		else if (find_neighbour(info, FLOWER) >= 0)
+			obstacle = 1;
+		else if (find_neighbour(info, OUTSIDE) >= 0)
+			obstacle = 1;
+		else if (find_neighbour(info, BEE_0) >= 0)
+			obstacle = 1;
+		else if (find_neighbour(info, BEE_0_WITH_FLOWER) >= 0)
+			obstacle = 1;
+		else if (find_neighbour(info, BEE_1) >= 0)
+			obstacle = 1;
+		else if (find_neighbour(info, BEE_1_WITH_FLOWER) >= 0)
+			obstacle = 1;
+		else if (find_neighbour(info, WALL) >= 0)
+			obstacle = 1;
 		if (obstacle >= 0)
 		{
 			return (command_t) {
